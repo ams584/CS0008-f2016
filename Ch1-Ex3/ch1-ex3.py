adafc2e5c05a7d436faad3c8eaff4aa24a08525a
ams584 <ams584@pitt.edu>
2016-09-06 14:56:11 -0400

    Ch1-Ex3

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,16 @@
+#
+# Template for code submission
+# name       : Alexander Shepard
+# email      : ams584@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+
