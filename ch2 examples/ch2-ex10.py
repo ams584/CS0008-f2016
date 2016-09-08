f324caede9f03a14eb833af5845e6b0a95272447
ams584 <ams584@pitt.edu>
2016-09-08 15:29:59 -0400

    Changed the format of the float input, and added an infinite loop

--- ch2 examples/ch2-ex10.py
+++ ch2 examples/ch2-ex10.py
@@ -12,19 +12,23 @@
 #
 # Notes:
 # any notes to the instructor and/or TA goes here
+x = 0
+while x == 0:
 
-s = 300     # Sugar
-b = 240     # Butter
-f = 330     # Flour
+    s = 300     # Sugar
+    b = 240     # Butter
+    f = 330     # Flour
 
-c = float(input("How many cookies do you want to make?"))
-p = c/48    # Amount of ingredients needed in relation to the base number of ingredients
+    c = float(\
+        input(\
+            "How many cookies do you want to make?"))
+    p = c/48    # Amount of ingredients needed in relation to the base number of ingredients
 
-S = float(s*p)  # Amount of sugar needed
-B = float(b*p)  # Amount of butter needed
-F = float(f*p)  # Amount of flour needed
+    S = float(s*p)  # Amount of sugar needed
+    B = float(b*p)  # Amount of butter needed
+    F = float(f*p)  # Amount of flour needed
 
-print("To make", c, "cookie(s), you need", S, "grams of sugar,", B, "grams of butter, and", F, "grams of flour")
+    print("To make", c, "cookie(s), you need", S, "grams of sugar,", B, "grams of butter, and", F, "grams of flour")
 
 
 
