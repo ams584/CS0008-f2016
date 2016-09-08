303f0b6f3aa2da556d4e1ec92532bc55825c87a7
ams584 <ams584@pitt.edu>
2016-09-08 12:50:13 -0400

    Defined the distance formula

---
+++ ch2 examples/ch2-ex5.py
@@ -0,0 +1,21 @@
+#
+# Template for code submission
+# name       : Alexander Shepard
+# email      : ams584@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+
+s = 90.0
+t = float(input("Enter the time in hours:"))
+d = s*t
+
+print(d)
