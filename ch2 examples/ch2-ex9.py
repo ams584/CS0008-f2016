69f4889f06b0439ea8806a8ec53573b002fe4475
ams584 <ams584@pitt.edu>
2016-09-08 14:57:42 -0400

    Asked for input in Fahrenheit and converted it to Celsius

---
+++ ch2 examples/ch2-ex9.py
@@ -0,0 +1,18 @@
+#
+# Template for code submission
+# name       : Alexander Shepard
+# email      : ams584@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, exercise 9
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+
+f = float(input("Enter the temperature in Fahrenheit"))
+c = ((f-32)*5)/9        # The formula for Celsius given Fahrenheit
+print("The temperature in Celsius is:", c)
