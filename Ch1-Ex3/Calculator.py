e0c1494a17380c5a09510248009b4a6dfcd60e29
ams584 <ams584@pitt.edu>
2016-09-13 15:24:27 -0400

    Looped calculations, added total for addition

--- Ch1-Ex3/Calculator.py
+++ Ch1-Ex3/Calculator.py
@@ -1,16 +1,22 @@
-n1 = float(input("Enter the first number:"))
-sign = input("Enter the operator:")
-n2 = float(input("Enter the second number:"))
-a = 0
-if sign == "+":
-    a = n1+n2
-    print(a)
-elif sign == "-":
-    a = n1-n2
-    print(a)
-elif sign == "*":
-    a = n1*n2
-    print(a)
-elif sign == "/":
-    a = n1/n1
-    print(a)
+l = 0
+b = 0
+while l <1:
+    n1 = float(input("Enter a number:"))
+    sign = input("Enter the operator:")
+    n2 = float(input("Enter another number:"))
+    a = 0
+    if sign == "+":
+        a = n1+n2
+        print(a)
+    elif sign == "-":
+        a = n1-n2
+        print(a)
+    elif sign == "*":
+        a = n1*n2
+        print(a)
+    elif sign == "/":
+        a = n1/n1
+        print(a)
+    total = a + b
+    b = total
+    print("Your total is:", total)
