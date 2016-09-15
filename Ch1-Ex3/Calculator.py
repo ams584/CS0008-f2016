fc5ff87a8601e65fa84cb3460958b091e0c6d527
ams584 <ams584@pitt.edu>
2016-09-15 12:12:32 -0400

    fixed total, condensed code

--- Ch1-Ex3/Calculator.py
+++ Ch1-Ex3/Calculator.py
@@ -1,22 +1,30 @@
 l = 0
 b = 0
-while l <1:
-    n1 = float(input("Enter a number:"))
-    sign = input("Enter the operator:")
-    n2 = float(input("Enter another number:"))
-    a = 0
-    if sign == "+":
-        a = n1+n2
-        print(a)
-    elif sign == "-":
-        a = n1-n2
-        print(a)
-    elif sign == "*":
-        a = n1*n2
-        print(a)
-    elif sign == "/":
-        a = n1/n1
-        print(a)
-    total = a + b
-    b = total
-    print("Your total is:", total)
+n1 = float(input("Enter a number:"))
+sign = input("Enter the operator:")
+n2 = float(input("Enter another number:"))
+a = 0
+if sign == "+":
+    a = n1+n2
+elif sign == "-":
+    a = n1-n2
+elif sign == "*":
+    a = n1*n2
+elif sign == "/":
+    a = n1/n1
+print(a)
+
+while l < 1:
+    b = a
+    sign2 = input("Enter an operator:")
+    n = float(input("Enter another number:"))
+    if sign2 == "+":
+        a = b + n
+    elif sign2 == "-":
+        a = b - n
+    elif sign2 == "*":
+        a = b * n
+    elif sign2 == "/":
+        a = b / n
+    print(a)
+
