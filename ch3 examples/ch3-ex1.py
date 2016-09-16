7eab1125a0218633edf94bc6c4a8a0b3b15b9e3f
ams584 <ams584@pitt.edu>
2016-09-16 13:54:46 -0400

    Created/Finished Program

---
+++ ch3 examples/ch3-ex1.py
@@ -0,0 +1,26 @@
+# name       : Alexander Shepard
+# email      : ams584@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 3, exercise 1
+
+n = int(input("Enter a number from 1-7:"))
+if n > 7 or n < 1:
+    print("Number must be between 1 and 7")
+if n == 1:
+    print("Monday")
+if n == 2:
+    print("Tuesday")
+if n == 3:
+    print("Wednesday")
+if n == 4:
+    print("Thursday")
+if n == 5:
+    print("Friday")
+if n == 6:
+    print("Saturday")
+if n == 7:
+    print("Sunday")
