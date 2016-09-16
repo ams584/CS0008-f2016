2aa6709de227ba1fe7ba6e3dc89855c5753eab03
ams584 <ams584@pitt.edu>
2016-09-16 14:04:18 -0400

    Added comments

--- ch3 examples/ch3-ex1.py
+++ ch3 examples/ch3-ex1.py
@@ -7,10 +7,10 @@
 # Description:
 # Starting with Python, Chapter 3, exercise 1
 
-n = int(input("Enter a number from 1-7:"))
+n = int(input("Enter a number from 1-7:"))  #Recieves a number from the user
 if n > 7 or n < 1:
-    print("Number must be between 1 and 7")
-if n == 1:
+    print("Number must be between 1 and 7") #Prints an error message if outside range
+if n == 1:  #Determines day of week
     print("Monday")
 if n == 2:
     print("Tuesday")
