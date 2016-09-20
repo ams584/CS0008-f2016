e1d428d4ce18054233586917f9aa1d4ece066f4e
ams584 <ams584@pitt.edu>
2016-09-19 21:29:17 -0400

    Started/finished program

--- ch3 examples/ch3-ex12.py
+++ ch3 examples/ch3-ex12.py
@@ -1,11 +1,13 @@
-w = float(input("Enter the weight of the package:"))    #Recieves the weight of the package
-c = 0   #Deifines the cost variable
-if w <= 2:  #Determines the cost of shipping based on the weight of the package
-    c = 1.50
-elif w > 2 and w < 6:
-    c = 3.00
-elif w > 6 and w < 10:
-    c = 4.00
-elif w > 10:
-    c = 4.75
-print("The cost of shipping is:", c, "dollars.")
+p = int(input("Enter the number of packages bought:"))
+d = 0
+if p >= 10 and p <= 19:
+    d = .10
+elif p >= 20 and p <= 49:
+    d = .20
+elif p >= 50 and p <= 99:
+    d = .30
+elif p >= 100:
+    d = .40
+discount = 99 * d
+c = 99 - discount
+print("Your discount is:", discount, "Your total cost is:", c)
