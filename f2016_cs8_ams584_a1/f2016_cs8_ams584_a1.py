5261fbf6d278ef7d14f17de9bf1453788e884ec2
ams584 <ams584@pitt.edu>
2016-09-29 14:47:35 -0400

    Added comments

--- f2016_cs8_ams584_a1/f2016_cs8_ams584_a1.py
+++ f2016_cs8_ams584_a1/f2016_cs8_ams584_a1.py
@@ -7,24 +7,24 @@
 # Description:
 # Starting with Python, Chapter 3, exercise 12
 
-units = str(input("Would you like to use USC or Metric?"))
-if units == "USC":
-    miles = float(input("Enter the number of miles driven:"))
-    gallons = float(input("Enter the number of gallons used:"))
+units = str(input("Would you like to use USC or Metric?"))                  # Asks for the user to select USC or Metric
+if units == "USC":                                                          # Runs if the user selected usc
+    miles = float(input("Enter the number of miles driven:"))               # Receives the number of miles
+    gallons = float(input("Enter the number of gallons used:"))             # Receives the number of gallons
     mpg = miles/gallons
-    kilometers = miles*1.60934
-    liters = gallons*3.78541
-    lkm = 100*liters/kilometers
+    kilometers = miles*1.60934                                              # Converts miles to kilometers
+    liters = gallons*3.78541                                                # Converts gallons to liters
+    lkm = 100*liters/kilometers                                             # Calculates liters per 100 km
 
-elif units == "Metric":
-    kilometers = float(input("Enter the number of kilometers driven:"))
-    liters = float(input("Enter the number of liters used:"))
-    lkm = 100*liters/kilometers
-    miles = kilometers*.621371
-    gallons = liters*.264172
-    mpg = miles/gallons
+elif units == "Metric":                                                     # Runs if the user selects Metric
+    kilometers = float(input("Enter the number of kilometers driven:"))     # Receives the number of kilometers
+    liters = float(input("Enter the number of liters used:"))               # Receives the number of liters
+    lkm = 100*liters/kilometers                                             # Calculates liters per 100 km
+    miles = kilometers*.621371                                              # Converts kilometers to miles
+    gallons = liters*.264172                                                # Converts liters to gallons
+    mpg = miles/gallons                                                     # Calculates miles per gallon
 
-if lkm > 20:
+if lkm > 20:                                                                # Tests the number of liters used
     cc = "Extremely Poor"
 elif lkm > 15 and lkm <= 20:
     cc = "Poor"
@@ -35,8 +35,8 @@ elif lkm > 8 and lkm <= 10:
 elif lkm <= 8:
     cc = "Excellent"
 
-print("                   USC:", "     Metric:")
-print("Distance_____:",   format(miles, '10.3f'), format(kilometers, '10.3f'))
-print("Gas__________:",    format(gallons, '10.3f'), format(liters, '10.3f'))
-print("Consumption__:",   format(mpg, '10.3f'), format(lkm, '10.3f'))
-print("Gas Consumption Rating:", cc)
+print("                   USC:", "     Metric:")                                    # Displays the titles
+print("Distance_____:",   format(miles, '10.3f'), format(kilometers, '10.3f'))      # Displays the distances
+print("Gas__________:",    format(gallons, '10.3f'), format(liters, '10.3f'))       # Displays the amount of gas
+print("Consumption__:",   format(mpg, '10.3f'), format(lkm, '10.3f'))               # Displays gas used over distance
+print("Gas Consumption Rating:", cc)                                                # Displays consumption rating
