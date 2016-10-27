9dc4913400b8b54ab9ab37ba8d130fa5b15f89c4
ams584 <ams584@pitt.edu>
2016-10-26 23:38:31 -0400

    Started setting up print

--- f2016_cs8_ams584_a2/f2016_cs8_ams584_a2.py
+++ f2016_cs8_ams584_a2/f2016_cs8_ams584_a2.py
@@ -4,15 +4,29 @@ while t == 0:
         x = str(input("Enter the name of the file and location: "))
         x = open(x, 'r')
         n = 0
+        distance_total = 0
         for line in x:
             n = n+1
             contents = (x.readline())
             name, distance = contents.split(',')
             if isinstance(distance, str):
-                distance_i = float(distance)
-            print(distance_i)
-        if line != '':
-            print("Number of lines in file: ", n)
-
-
+                distance = float(distance)
+            distance_total = distance_total + distance
+        c = 0
+        while c == 0:
+            y = str(input("Read another file? 'y' or 'n': "))
+            if y == 'n':
+                t = 1
+                c = 1
+                print("File to be read: ", x)
+                if line != '':
+                    print("Number of lines in file: ", n)
+                print("Total distance run: ", distance_total)
+            elif y == 'y':
+                t = 0
+                c = 1
+            else:
+                c = 0
+                Y = str(input("That was not a valid input."))
     processFile()
+
