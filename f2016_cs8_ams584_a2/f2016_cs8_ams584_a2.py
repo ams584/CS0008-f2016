9c104e7792b461a1c19c09d51cad909e8d37bdb5
ams584 <ams584@pitt.edu>
2016-10-27 13:30:45 -0400

    Total distance working

--- f2016_cs8_ams584_a2/f2016_cs8_ams584_a2.py
+++ f2016_cs8_ams584_a2/f2016_cs8_ams584_a2.py
@@ -1,32 +1,29 @@
-t = 0
-while t == 0:
+def main():
     def processFile():
         x = str(input("Enter the name of the file and location: "))
         x = open(x, 'r')
         n = 0
-        distance_total = 0
+        distance_x = 0
         for line in x:
             n = n+1
             contents = (x.readline())
             name, distance = contents.split(',')
             if isinstance(distance, str):
                 distance = float(distance)
-            distance_total = distance_total + distance
-        c = 0
-        while c == 0:
-            y = str(input("Read another file? 'y' or 'n': "))
-            if y == 'n':
-                t = 1
-                c = 1
-                print("File to be read: ", x)
-                if line != '':
-                    print("Number of lines in file: ", n)
-                print("Total distance run: ", distance_total)
-            elif y == 'y':
-                t = 0
-                c = 1
-            else:
-                c = 0
-                Y = str(input("That was not a valid input."))
-    processFile()
-
+            distance_x = distance_x + distance
+        print(distance_x)
+        return(distance_x)
+    t = 0
+    distance_total = 0
+    while t == 0:
+        variable = processFile()
+        y = str(input("Read another file? 'y' or 'n': "))
+        if y == 'y':
+            distance_total = variable + distance_total
+        elif y == 'n':
+            distance_total = distance_total + variable
+            print("Total distance run: ", variable)
+            print(distance_total)
+        else:
+            Y = str(input("That was not a valid input."))
+main()
