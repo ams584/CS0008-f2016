dc1b9b84129741a888b297f32aed4f80f59c6795
ams584 <ams584@pitt.edu>
2016-11-17 00:52:41 -0500

    Started the project

--- f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
+++ f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
@@ -4,13 +4,16 @@ def processfile(master_file_list):
     length_mfl = len(master_file_list)
     for line in master_file_list:
         files = (master_file_list.readline())
-        files2 = line.append[files.rstrip('\n')]
-    return(files2, length_mfl)
-def process(files2):
-    processfile()
-    contents_file2 = open(files2, 'r')
-    for line in files2:
-        data = files2.readline()
+        files2 = files.split(',')
+        files3 = line.append[files.rstrip('\n')]
+    print(length_mfl, files3)
+    return(files3, length_mfl)
+def process(files3):
+    files3, length_mfl = processfile()
+    length_lines = len(files3) * length_mfl
+    contents_file2 = open(files3, 'r')
+    for line in files3:
+        data = files3.readline()
         name, distance = data.split(',')
-        
 
+process()
