bd83972a810bf7bc52eac600975902ac8b63db02
ams584 <ams584@pitt.edu>
2016-11-17 12:44:24 -0500

    Started the project

--- f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
+++ f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
@@ -1,19 +1,18 @@
+master_file_list = (input("Enter the location of the file list: "))
 def processfile(master_file_list):
-    master_file_list = str(input("Enter the location of the file list: "))
     contents_mfl = open(master_file_list, 'r')
     length_mfl = len(master_file_list)
-    for line in master_file_list:
-        files = (master_file_list.readline())
+    for line in contents_mfl:
+        files = (contents_mfl.readline())
         files2 = files.split(',')
-        files3 = line.append[files.rstrip('\n')]
-    print(length_mfl, files3)
-    return(files3, length_mfl)
-def process(files3):
-    files3, length_mfl = processfile()
-    length_lines = len(files3) * length_mfl
-    contents_file2 = open(files3, 'r')
-    for line in files3:
-        data = files3.readline()
+    print(length_mfl, files2)
+    return(files2, length_mfl)
+processfile(master_file_list)
+def process(files2):
+    length_lines = len(files2)
+    contents_file2 = open(files2, 'r')
+    for line in files2:
+        data = files2.readline()
         name, distance = data.split(',')
 
 process()
