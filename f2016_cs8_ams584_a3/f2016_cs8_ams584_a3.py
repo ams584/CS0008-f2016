3e71f1fa147313cd2ea4dbf0376604abcc62a97b
ams584 <ams584@pitt.edu>
2016-11-18 13:31:37 -0500

    Mostly complete, output not working

--- f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
+++ f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
@@ -1,24 +1,35 @@
-master_file_list = (input("Enter the location of the file list: "))
-def processfile(master_file_list):
+master_file_list = input("Enter the location of the file list: ")
+global_dict = {}
+def processfile(master_file_list, global_dict):
     contents_mfl = open(master_file_list, 'r')
-    files = (contents_mfl.readlines())
+    files = contents_mfl.readlines()
     for line in contents_mfl:
         filename = line.rstrip('\n')
         length_mfl = len(files)
-        print(filename)
         x = open(filename, 'r')
         x.readline()
         for line in x:
-            print(line)
             line = line.rstrip('\n')
             name, dist = line.split(',')
             p_dist = int(dist)
             t_dist = 0
             t_dist += p_dist
-            p_names = int(name)
-            t_names = 0
-            t_names += p_names
-            if name in list:
-                list = list.append(line, list)
-        print(p_dist, p_names)
-processfile(master_file_list)
+            if name in dict:
+                dict[name] = dict[name].append(dist)
+    return(dict, t_dist, length_mfl)
+
+processfile(master_file_list, global_dict)
+max_name = ' '
+max_dist = 0
+for name in global_dict:
+    l_values = global_dict[name]
+    if (max(l_values) > max_dist):
+        max_dist = max(l_values)
+        max_name = name
+min_name = ' '
+min_dist = 0
+for name in global_dict:
+    x_values = global_dict[name]
+    if (min(x_values) < min_dist):
+        min_dist = min(x_values)
+        min_name = name
