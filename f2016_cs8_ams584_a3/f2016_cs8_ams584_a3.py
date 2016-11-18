7d54c8d78a2e2b6cb55f88201100631e6ec99b72
ams584 <ams584@pitt.edu>
2016-11-18 09:54:35 -0500

    Output not working

--- f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
+++ f2016_cs8_ams584_a3/f2016_cs8_ams584_a3.py
@@ -1,18 +1,24 @@
 master_file_list = (input("Enter the location of the file list: "))
 def processfile(master_file_list):
     contents_mfl = open(master_file_list, 'r')
-    length_mfl = len(master_file_list)
-    for line in contents_mfl:
-        files = (contents_mfl.readline())
-        files2 = files.split(',')
-    print(length_mfl, files2)
-    return(files2, length_mfl)
-processfile(master_file_list)
-def process(files2):
-    length_lines = len(files2)
-    contents_file2 = open(files2, 'r')
-    for line in files2:
-        data = files2.readline()
-        name, distance = data.split(',')
-
-process()
+    files = (contents_mfl.readlines())
+    for line in files:
+        filename = line.rstrip('\n')
+        length_mfl = len(files)
+        print(filename)
+        x = open(filename, 'r')
+        contents = x.readline()
+        name, dist = contents.split('\n')
+        print(contents)
+        list = [name]
+        for line in contents:
+            contents.rstrip('\n')
+            p_dist = int(dist)
+            t_dist = 0
+            t_dist += p_dist
+            p_names = int(name)
+            t_names = 0
+            t_names += p_names
+            if name in list:
+                list = list.append(name, list)
+        print(p_dist, p_names)
