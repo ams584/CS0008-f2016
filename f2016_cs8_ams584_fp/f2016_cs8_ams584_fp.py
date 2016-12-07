368a1a63bc5aed4c877919568bd0faadb2a0e4bc
ams584 <ams584@pitt.edu>
2016-12-06 20:08:10 -0500

    Created the processData function

---
+++ f2016_cs8_ams584_fp/f2016_cs8_ams584_fp.py
@@ -0,0 +1,18 @@
+def processData(file):
+    dictionary_1 = {}   # Creates a dictionary
+    n_lines = 0
+    datafile = open(file, 'r')   # Opens the master file
+    datafile.readline()   # Reads the master file
+    for line in datafile:
+        if 'distance' in line:  # Skips the header line
+            continue
+        n_lines += 1    # Counts the number of files in the master file
+        line.rstrip('\n')   # Strips the \n from each line
+        name, dist = line.split(',')    # Splits each line into the name and the distance
+        if not name in dictionary_1:
+            dictionary_1.append({'name': name, 'distance': float(dist)})    # Appends the name and distance for each line if it's not in the dictionary already
+        return(dictionary_1, n_lines)
+    
+
+
+
