26c61618d9657b9c446ee8df22449d95e87f683c
ams584 <ams584@pitt.edu>
2016-12-06 20:24:00 -0500

    Created the processData function

--- Random/Test.py
+++ Random/Test.py
@@ -1,27 +1,9 @@
-def processFile():
-    x = str(input("Enter the name of the file: "))
-    x = open('/users/Alex/Desktop/Data_File_1.txt', 'r')
-    n_1 = 0
-    for line in Data_File_1:
-        n_1 = n_1+1
-        contents_f1 = (Data_File_1.readline())
-        name_f1, distance_f1 = contents_f1.split(',')
-        if isinstance(distance_f1, str):
-            distance_f1 = float(distance_f1)
-        print(distance_f1)
-    if line != '':
-        print("Number of lines in file 1:", n_1)
-    Data_File_2 = open('/users/Alex/Desktop/Data_File_2.txt', 'r')
-    n_2 = 0
-    for line in Data_File_2:
-        n_2 = n_2 + 1
-        contents_f2 = (Data_File_2.readline())
-        name_f2, distance_f2 = contents_f2.split(',')
-        if isinstance(distance_f2, str):
-            distance_f2 = float(distance_f2)
-        print(distance_f2)
-    if line != '':
-        print("Number of lines in file 2:", n_2)
+import tkinter as tk
+root = tk.Tk()
 
+def click1(event):
+    x, y = event.x, event.y
+    print(x, y)
 
-processFile()
+root.bind('<Button-1>', click1)
+root.mainloop()
--- f2016_cs8_ams584_fp/f2016_cs8_ams584_fp.py
+++ f2016_cs8_ams584_fp/f2016_cs8_ams584_fp.py
@@ -12,7 +12,16 @@ def processData(file):
         if not name in dictionary_1:
             dictionary_1.append({'name': name, 'distance': float(dist)})    # Appends the name and distance for each line if it's not in the dictionary already
         return(dictionary_1, n_lines)
-    
+masterFile = input("Enter the name of the master file: ")   # Recieves the master file list
+for line in masterFile:
+    line.rstrip('\n')
+t_dist = 0
+for item in processData[0]:
+    p_dist = 'distance'
+    t_dist = t_dist + p_dist    # Totals the distances
+
+
+
 
 
 
