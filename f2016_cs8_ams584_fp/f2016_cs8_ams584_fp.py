4e3cc916bf9d23671bc08a6ba0728dde6b9d7fa2
ams584 <ams584@pitt.edu>
2016-12-15 20:22:56 -0500

    Still need to add comments

--- f2016_cs8_ams584_fp/f2016_cs8_ams584_fp.py
+++ f2016_cs8_ams584_fp/f2016_cs8_ams584_fp.py
@@ -1,27 +1,136 @@
-def processData(file):
-    dictionary_1 = {}   # Creates a dictionary
-    n_lines = 0
-    datafile = open(file, 'r')   # Opens the master file
-    datafile.readline()   # Reads the master file
-    for line in datafile:
-        if 'distance' in line:  # Skips the header line
+class participant:      # Creates the participant class
+    name = "unknown"
+    distance = 0    # Defines the distance variable
+    runs = 0    # Defines the runs variable
+    def __init__(self, name, distance=0):   # Initiates the class
+        self.name = name
+        if distance > 0:
+            self.distance = distance
+            self.runs = 1
+    def addDistance(self, distance):    # Creates a function to add distances in a file
+        if distance > 0:
+            self.distance += distance
+            self.runs += 1
+    def addDistances(self, distances):   # Creates a function to add all the individual distances
+        for distance in distances:
+            if distance > 0:
+                self.distance += distance
+                self.runs += 1
+    def getName(self):  # Returns the name of the participant
+        return self.name
+
+    def getDistance(self):  #  Returns the particpants distance
+        return self.distance
+
+    def getRuns(self):  # Returns the number of runs for each particpant
+        return self.runs
+
+    def __str__(self):  # Formats the output
+        return \
+            "Name : " + format(self.name, '>20s') + \
+            ". Distance run : " + format(self.distance, '9.4f') + \
+            ". Runs : " + format(self.runs, '4d')
+
+    def tocsv(self):    #
+        return ','.join([self.name, str(self.runs), str(self.distance)])
+
+
+def getData(file):
+    output = []
+    for line in open(file,'r'):
+        if "distance" in line:
             continue
-        n_lines += 1    # Counts the number of files in the master file
-        line.rstrip('\n')   # Strips the \n from each line
-        name, dist = line.split(',')    # Splits each line into the name and the distance
-        if not name in dictionary_1:
-            dictionary_1.append({'name': name, 'distance': float(dist)})    # Appends the name and distance for each line if it's not in the dictionary already
-        return(dictionary_1, n_lines)
-masterFile = input("Enter the name of the master file: ")   # Recieves the master file list
-for line in masterFile:
-    line.rstrip('\n')
-t_dist = 0
-for item in processData[0]:
-    p_dist = 'distance'
-    t_dist = t_dist + p_dist    # Totals the distances
+        line = line.rstrip('\n').split(',')
+        try:
+            output.append({'name': line[0].strip(' '), 'distance':float(line[1])})
+        except:
+            print('Invalid row : '+line.rstrip('\n'))
+    return output
+masterFile = input("Enter the Master File : ")
 
+try:
+   data = open(masterFile, 'r')
+   for file in masterFile:
+       dataFiles = file.rstrip('\n')
+except:
+    print("Invalid file name")
+    exit(1)
 
+for data in dataFiles:
+    rawData = sum(getData(file))
+nfiles = len(dataFiles)
 
+totalLines = len(rawData)
 
+totalDistance = sum([item['distance'] for item in rawData])
+
+participants = {}
+
+for item in rawData:
+
+    if not item['name'] in participants.keys():
+        participants[item['name']] = participant(item['name'])
+    participants[item['name']].addDistance(item['distance'])
+
+minDistance = { 'name' : None, 'distance': None }
+
+maxDistance = { 'name' : None, 'distance': None }
+
+apparences = {}
+
+for name, object in participants.items():
+
+    distance = object.getDistance()
+
+    if minDistance['name'] is None or minDistance['distance'] > distance:
+        minDistance['name'] = name
+        minDistance['distance'] = distance
+
+    if maxDistance['name'] is None or maxDistance['distance'] < distance:
+        maxDistance['name'] = name
+        maxDistance['distance'] = distance
+
+    participantAppearences = object.getRuns()
+
+    if not participantAppearences in apparences.keys():
+        apparences[participantAppearences] = []
+    apparences[participantAppearences].append(name)
+
+totalParticipants = len(participants);
+
+totalParticipantsMultipleRecords = len([1 for item in participants.values() if item.getRuns() > 1])
+
+int = '5d'
+float = '12.5f'
+str = '20s'
+
+print("")
+print(" Number of Input files read   : " + format(nfiles,int))
+print(" Total number of lines read   : " + format(totalLines,int))
+print("")
+print(" Total distance run           : " + format(totalDistance,float))
+print("")
+print(" Max distance run             : " + format(maxDistance['distance'],float))
+print("   by participant             : " + format(maxDistance['name'],str))
+print("")
+print(" Min distance run             : " + format(minDistance['distance'],float))
+print("   by participant             : " + format(minDistance['name'],str))
+print("")
+print(" Total number of participants : " + format(totalParticipants,int))
+print(" Number of participants")
+print("  with multiple records       : " + format(totalParticipantsMultipleRecords,float))
+print("")
+
+outputFile = "f2016_cs8_ams584_fp.output.csv"
+
+fh = open(outputFile,'w')
+
+fh.write('name,records,distance\n')
+
+for name, object in participants.items():
+
+    fh.write(object.tocsv() + '\n')
+
+fh.close()
 
 
